96b0a71846920cb705567ca61228620aeef52667
akhil0778 <akhil0778358@gmail.com>
2026-01-01 19:57:14 +0530

    feat: add jwt protected user profile route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const dotenv=require('dotenv');
-const authRoutes=require('./routes/authRoutes')
+const authRoutes=require('./routes/authRoutes');
+const userRoutes=require('./routes/userRoutes');
 const createTables=require('./database/schema');
 
 dotenv.config();
@@ -11,6 +12,7 @@ const PORT=process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/auth",authRoutes);
+app.use('/api/users',userRoutes);
 
 async function startServer() {
     try{
